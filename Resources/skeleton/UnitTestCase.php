bb939195632ec92c2554e9c552cdb089780ad00c
Nicolas Paris <nicolas.caen@gmail.com>
2011-04-18 07:53:09 +0200

    tearDown and setUp added to template

--- Resources/skeleton/UnitTestCase.php
+++ Resources/skeleton/UnitTestCase.php
@@ -4,6 +4,16 @@
 
 class {{ nameClass }} extends \PHPUnit_Framework_TestCase
 {
+    protected function setUp ()
+    {
+        
+    }
+
+    protected function tearDown ()
+    {
+        
+    }
+
     public function testIndex ()
     {
         
--- Resources/skeleton/WebTestCase.php
+++ Resources/skeleton/WebTestCase.php
@@ -6,6 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class {{ className }} extends WebTestCase
 {
+    protected function setUp ()
+    {
+        
+    }
+
+    protected function tearDown ()
+    {
+        
+    }
+
     public function testIndex ()
     {
 
